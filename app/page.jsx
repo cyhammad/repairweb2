dcf9b84d911d2a0f4da0733e5fa7247cf063810e
cyhammad <hammad727272@gmail.com>
2023-06-07 01:02:31 +0500

    hero section added

--- app/page.jsx
+++ app/page.jsx
@@ -1,30 +1,147 @@
 import Slider from "@/components/slider/Slider";
 import StatsBar from "@/components/stats/StatsBar";
 import Image from "next/image";
+import {
+  CheckBadgeIcon,
+  WrenchScrewdriverIcon,
+} from "@heroicons/react/24/outline";
+
+import { PhoneIcon, EnvelopeIcon } from "@heroicons/react/24/solid";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex w-full justify-between items-center">
-        <div className="flex flex-col">
-          <div className="pr-10">
-            <p className="text-4xl">A technician you can trust</p>
-            <p className="mt-3">
-              When it comes to repairing your home appliances, you need a
-              technician you can trust to deliver exceptional service. With our
-              extensive knowledge, expertise, and commitment to customer
-              satisfaction, we are dedicated to providing you with the highest
-              quality repairs and ensuring your appliances work flawlessly.
-            </p>
-          </div>
-          <div className="px-5 pt-10">
-            <StatsBar />
+    <main>
+      {/* HERO SECTION 1 */}
+      <div
+        className="hero min-h-[90vh]"
+        style={{ backgroundImage: "url('/images/slider/img-1.jpg')" }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content md:w-full text-center md:text-left text-white">
+          <div className="w-full">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-5xl font-bold">
+                A technician you can trust
+              </h1>
+              <p className="mb-5">
+                When it comes to repairing your home appliances, you need a
+                technician you can trust to deliver exceptional service.
+              </p>
+              <button className="btn btn-primary">Explore More</button>
+            </div>
           </div>
         </div>
-        <div className="w-full">
-          <Slider />
+      </div>
+      {/* HERO SECTION 2 */}
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="grid grid-cols-2 grid-rows-2 gap-5">
+            <div className="max-w-xs flex justify-end items-end">
+              <img
+                src="/images/tv-repair.png"
+                className="max-w-[6rem] md:max-w-[15rem] shadow-2xl"
+              />
+            </div>
+            <div className="max-w-xs flex justify-start items-end">
+              <img
+                src="/images/washing-machine-repair.png"
+                className="max-w-[7rem] md:max-w-[19rem] shadow-2xl"
+              />
+            </div>
+            <div className="max-w-xs flex justify-end items-start">
+              <img
+                src="/images/cooker-repair.png"
+                className="max-w-[4rem] md:max-w-[12rem] shadow-2xl"
+              />
+            </div>
+            <div className="max-w-xs flex justify-start items-start">
+              <img
+                src="/images/fridge-repair.png"
+                className="max-w-[5.5rem] md:max-w-[16rem] shadow-2xl"
+              />
+            </div>
+          </div>
+          <div>
+            <h1 className="text-5xl font-bold max-w-xl">
+              Welcome To The Best Home Appliance Repair
+            </h1>
+            <div className="flex gap-10 pt-5 flex-col sm:flex-row">
+              <div className="flex items-center gap-1">
+                <WrenchScrewdriverIcon className="h-10 w-10 text-primary" />
+                <div className="text-lg font-bold">Expert Technician</div>
+              </div>
+              <div className="flex items-center gap-1">
+                <CheckBadgeIcon className="h-12 w-12 text-primary" />
+                <div className="text-lg font-bold">Best Service Quality</div>
+              </div>
+            </div>
+            <p className="py-6 max-w-xl">
+              With our extensive knowledge, expertise, and commitment to
+              customer satisfaction. We are dedicated to providing you with the
+              highest quality repairs and ensuring your appliances work
+              flawlessly.
+            </p>
+            <div className="divider"></div>
+            <div className="flex gap-10 flex-col sm:flex-row">
+              <div className="flex items-center gap-2">
+                <div className="p-4 bg-primary rounded-full">
+                  <PhoneIcon className="h-6 w-6 text-white" />
+                </div>
+                <div className="text-lg font-bold">+913562225555</div>
+              </div>
+              <div className="flex items-center gap-2">
+                <div className="p-4 bg-primary rounded-full">
+                  <EnvelopeIcon className="h-6 w-6 text-white" />
+                </div>
+                <div className="text-lg font-bold">info@example.com</div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </main>
   );
+}
+{
+  /* <div className="hero bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img
+            src="/images/slider/img-1.jpg"
+            className="max-w-sm rounded-lg shadow-2xl"
+          />
+          <div>
+            <h1 className="text-5xl font-bold">Box Office News!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div> */
+}
+{
+  /* <div className="max-w-7xl flex min-h-screen flex-col items-center justify-between">
+        <div className="flex w-full justify-between items-center">
+          <div className="flex flex-col">
+            <div className="pr-10">
+              <p className="text-4xl">A technician you can trust</p>
+              <p className="mt-3">
+                When it comes to repairing your home appliances, you need a
+                technician you can trust to deliver exceptional service. With
+                our extensive knowledge, expertise, and commitment to customer
+                satisfaction, we are dedicated to providing you with the highest
+                quality repairs and ensuring your appliances work flawlessly.
+              </p>
+            </div>
+            <div className="px-5 pt-10">
+              <StatsBar />
+            </div>
+          </div>
+          <div className="w-full">
+            <Slider />
+          </div>
+        </div>
+      </div> */
 }
--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { PhoneIcon } from "@heroicons/react/24/solid";
+import { PhoneIcon, WrenchScrewdriverIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 
 const Header = () => {
@@ -16,110 +16,114 @@ const Header = () => {
           +91 9876543210
         </div>
       </Link>
-      <div className="navbar bg-primary text-black font-raleway">
-        <div className="navbar-start">
-          <div className="dropdown m-0">
-            <label tabIndex={0} className="btn btn-ghost px-2 lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+      <div className="bg-primary flex justify-center text-black font-raleway">
+        <div className="navbar max-w-7xl">
+          <div className="navbar-start">
+            <div className="dropdown m-0">
+              <label tabIndex={0} className="btn btn-ghost px-2 lg:hidden">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </label>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box z-50"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </label>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box"
-            >
+                <li>
+                  <a>Home</a>
+                </li>
+                <li>
+                  <a>Services</a>
+                  <ul className="p-2">
+                    <li>
+                      <a>Washing Machine Repair</a>
+                    </li>
+                    <li>
+                      <a>Refrigerator Repair</a>
+                    </li>
+                    <li>
+                      <a>Dishwasher Repair</a>
+                    </li>
+                    <li>
+                      <a>TV Repair</a>
+                    </li>
+                    <li>
+                      <a>Dryer Repair</a>
+                    </li>
+                    <li>
+                      <a>Stove / Cooker Repair</a>
+                    </li>
+                    <li>
+                      <a>Gas & Electric Oven Repair</a>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a>Contact Us</a>
+                </li>
+              </ul>
+            </div>
+            <a className="btn btn-ghost normal-case text-lg hidden">
+              <WrenchScrewdriverIcon className="h-6 w-6 text-black" />
+              Best Appliances Repair
+            </a>
+            <a className="btn btn-ghost normal-case text-lg px-1">
+              <WrenchScrewdriverIcon className="h-6 w-6 text-black" />
+              Best Appliances Repair
+            </a>
+          </div>
+          <div className="navbar-end hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">
               <li>
                 <a>Home</a>
               </li>
               <li>
-                <a>Services</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Washing Machine Repair</a>
-                  </li>
-                  <li>
-                    <a>Refrigerator Repair</a>
-                  </li>
-                  <li>
-                    <a>Dishwasher Repair</a>
-                  </li>
-                  <li>
-                    <a>TV Repair</a>
-                  </li>
-                  <li>
-                    <a>Dryer Repair</a>
-                  </li>
-                  <li>
-                    <a>Stove / Cooker Repair</a>
-                  </li>
-                  <li>
-                    <a>Gas & Electric Oven Repair</a>
-                  </li>
-                </ul>
+                <a>About Us</a>
+              </li>
+              <li tabIndex={0}>
+                <details>
+                  <summary>Services</summary>
+                  <ul className="p-2">
+                    <li>
+                      <a>Washing Machine Repair</a>
+                    </li>
+                    <li>
+                      <a>Refrigerator Repair</a>
+                    </li>
+                    <li>
+                      <a>Dishwasher Repair</a>
+                    </li>
+                    <li>
+                      <a>TV Repair</a>
+                    </li>
+                    <li>
+                      <a>Dryer Repair</a>
+                    </li>
+                    <li>
+                      <a>Stove / Cooker Repair</a>
+                    </li>
+                    <li>
+                      <a>Gas & Electric Oven Repair</a>
+                    </li>
+                  </ul>
+                </details>
               </li>
               <li>
                 <a>Contact Us</a>
               </li>
             </ul>
           </div>
-          <a className="btn btn-ghost normal-case text-lg hidden">
-            Best Appliances Repair
-          </a>
-          <a className="btn btn-ghost normal-case text-lg px-1">
-            Best Appliances Repair
-          </a>
-        </div>
-        <div className="navbar-end hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Home</a>
-            </li>
-            <li>
-              <a>About Us</a>
-            </li>
-            <li tabIndex={0}>
-              <details>
-                <summary>Services</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Washing Machine Repair</a>
-                  </li>
-                  <li>
-                    <a>Refrigerator Repair</a>
-                  </li>
-                  <li>
-                    <a>Dishwasher Repair</a>
-                  </li>
-                  <li>
-                    <a>TV Repair</a>
-                  </li>
-                  <li>
-                    <a>Dryer Repair</a>
-                  </li>
-                  <li>
-                    <a>Stove / Cooker Repair</a>
-                  </li>
-                  <li>
-                    <a>Gas & Electric Oven Repair</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Contact Us</a>
-            </li>
-          </ul>
         </div>
       </div>
     </header>
