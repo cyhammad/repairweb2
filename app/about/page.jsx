91e669ed2b349cd1929e71af4dd07641eec75c46
cyhammad <hammad727272@gmail.com>
2023-06-22 21:45:25 +0500

    about us image fix

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -5,7 +5,7 @@ const AboutPage = () => {
   return (
     <div
       className="hero min-h-screen bg-fixed"
-      style={{ backgroundImage: "url(/images/aboutus.jpg);" }}
+      style={{ backgroundImage: "url('/images/aboutus.jpg');" }}
     >
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-base-100 bg-blend-darken bg-black bg-opacity-60">
